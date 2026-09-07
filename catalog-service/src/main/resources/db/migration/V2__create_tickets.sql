00fdb73353cdb86d785cc7643411d472ac21ba20
Sergey Kremnev <sergeykremnev8888@gmail.com>
2026-09-07 14:55:30 +0300

    (catalog) fixed migration script

--- catalog-service/src/main/resources/db/migration/V2__create_tickets.sql
+++ catalog-service/src/main/resources/db/migration/V2__create_tickets.sql
@@ -1,18 +1,18 @@
 CREATE TABLE tickets
 (
-    id              UUID           NOT NULL,
-    event_id        UUID           NOT NULL,
-    seat_number     VARCHAR(50)    NOT NULL,
+    id              UUID          NOT NULL,
+    event_id        UUID          NOT NULL,
+    seat_number     VARCHAR(50)   NOT NULL,
     price           NUMERIC(19, 2) NOT NULL,
 
-    status          VARCHAR(20)    NOT NULL DEFAULT 'AVAILABLE',
+    status          VARCHAR(20)   NOT NULL DEFAULT 'AVAILABLE',
     reservation_id  UUID,
-    reserved_until  TIMESTAMPTZ,
+    reserved_until  TIMESTAMP,
 
-    version         BIGINT         NOT NULL DEFAULT 0,
+    version         BIGINT        NOT NULL DEFAULT 0,
 
-    created_at      TIMESTAMPTZ    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMPTZ    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT pk_tickets PRIMARY KEY (id),
 
