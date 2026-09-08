2140c23a37a35054a74187b0c4fb70b7fa5e0032
Sergey Kremnev <sergeykremnev8888@gmail.com>
2026-09-08 19:14:18 +0300

    feat(notification): add persistent Kafka consumer idempotency

--- notification-service/src/main/java/ru/ticketcraft/NotificationApplication.java
+++ notification-service/src/main/java/ru/ticketcraft/NotificationApplication.java
@@ -2,8 +2,6 @@ package ru.ticketcraft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 /**
  * Главный класс для запуска микросервиса уведомлений (Notification Service)
@@ -11,8 +9,6 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
  * Работает в чисто фоновом режиме, обрабатывая события из Apache Kafka.
  */
 @SpringBootApplication
-@EnableJdbcRepositories(basePackages = "ru.ticketcraft.repository")
-@ConfigurationPropertiesScan
 public class NotificationApplication {
 
     public static void main(String[] args) {
